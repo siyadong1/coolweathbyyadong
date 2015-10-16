dc01d7f71400c4899430e5a7825653c2ba4ab29a
siyadong <306704986@qq.com>
2015-10-16 23:15:02 +0800

    加入显示天气功能

--- src/com/coolweather/apps/activity/ChooseAreaActivity.java
+++ src/com/coolweather/apps/activity/ChooseAreaActivity.java
@@ -14,7 +14,10 @@ import com.coolweather.apps.util.Utility;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -74,6 +77,17 @@ public class ChooseAreaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_selected", false)) {
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		titleText = (TextView) findViewById(R.id.title_text);
@@ -94,6 +108,13 @@ public class ChooseAreaActivity extends Activity {
 				else if (currentLevel == LEVEL_CITY) {
 					selectedCity = cityList.get(arg2);
 					queryCounties(); //加载县数据
+				}else if (currentLevel == LEVEL_COUNTY) {
+					String countyCode = countyList.get(arg2).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
+					
 				}
 			}
 		});
--- src/com/coolweather/apps/util/Utility.java
+++ src/com/coolweather/apps/util/Utility.java
@@ -1,5 +1,15 @@
 package com.coolweather.apps.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -36,19 +46,21 @@ public class Utility {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 解析和处理服务器返回的市级数据
+	 * 
 	 * @param coolWeatherDB
 	 * @param response
 	 * @param provinecId
 	 * @return
 	 */
-	public synchronized static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB,String response,int provinecId) {
+	public synchronized static boolean handleCitiesResponse(
+			CoolWeatherDB coolWeatherDB, String response, int provinecId) {
 		if (!TextUtils.isEmpty(response)) {
 			String[] allCities = response.split(",");
 			if (allCities != null && allCities.length > 0) {
-				for (String p: allCities) {
+				for (String p : allCities) {
 					String[] array = p.split("\\|");
 					City city = new City();
 					city.setCityCode(array[0]);
@@ -56,7 +68,7 @@ public class Utility {
 					city.setProvinceId(provinecId);
 					coolWeatherDB.saveCity(city);
 				}
-				
+
 				return true;
 			}
 		}
@@ -65,17 +77,19 @@ public class Utility {
 
 	/**
 	 * 解析和处理服务器返回的县级数据
+	 * 
 	 * @param coolWeatherDB
 	 * @param response
 	 * @param cityId
 	 * @return
 	 */
-	
-	public synchronized static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,String response,int cityId) {
+
+	public synchronized static boolean handleCountiesResponse(
+			CoolWeatherDB coolWeatherDB, String response, int cityId) {
 		if (!TextUtils.isEmpty(response)) {
 			String[] cities = response.split(",");
 			if (cities != null && cities.length > 0) {
-				for (String p:cities) {
+				for (String p : cities) {
 					String[] array = p.split("\\|");
 					County county = new County();
 					county.setCountyCode(array[0]);
@@ -85,6 +99,65 @@ public class Utility {
 				}
 				return true;
 			}
-		}return false;
+		}
+		return false;
 	}
+
+	/**
+	 * 解析服务器返回的JSON数据，并将解析出的数据存储到本地
+	 * 
+	 * @param context
+	 * @param response
+	 */
+
+	public static void handleWeatherResponse(Context context, String response) {
+
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfor = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfor.getString("city");
+			String weatherCode = weatherInfor.getString("cityid");
+			String temp1 = weatherInfor.getString("temp1");
+			String temp2 = weatherInfor.getString("temp2");
+			String weatherDesp = weatherInfor.getString("weather");
+			String publishTime = weatherInfor.getString("ptime");
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+					weatherDesp, publishTime);
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 将服务器返回的所有天气信息存储到SharePreferences文件中
+	 * 
+	 * @param context
+	 * @param cityName
+	 * @param weatherCode
+	 * @param temp1
+	 * @param temp2
+	 * @param weatherDesp
+	 * @param publishTime
+	 */
+
+	public static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager
+				.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
+
+	}
+
 }
